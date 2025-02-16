41dfad6827aada4088e03cd9759b2340d62b4ba5
fujiisui <sui_face@yahoo.co.jp>
2025-02-16 14:42:15 +0900

    2: ルーティングの設定 #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
-  
-  resources :users, only: [:index, :edit, :update, :destroy]
-  resources :items, only: [:index, :show, :new, :create]
 
-  root 'home#index'
+  resources :users, only: [:index, :edit, :update, :destroy]
+  resources :items
+
+  root "home#index"
 end
