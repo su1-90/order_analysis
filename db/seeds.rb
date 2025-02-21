0f2e0d1764e1475d70e8dd78ed8f22f689f531e7
fujiisui <sui_face@yahoo.co.jp>
2025-02-21 10:56:54 +0900

    rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# User seeds
+users_data = [
+  { user_code: 'admin02', password: 'password', password_confirmation: 'password', admin: true },
+  { user_code: 'admin03', password: 'password', password_confirmation: 'password', admin: true },
+  { user_code: 'user02', password: 'password', password_confirmation: 'password', admin: false },
+  { user_code: 'user03', password: 'password', password_confirmation: 'password', admin: false },
+  { user_code: 'user04', password: 'password', password_confirmation: 'password', admin: false }
+]
+
+users_data.each do |user_data|
+  User.create!(user_data)
+end
+
+# Item seeds
+items_data = [
+  { product_code: '111111', name: 'サンプル弁当', order_date: '2025-02-10', order_quantity: 790 },
+  { product_code: '111111', name: 'サンプル弁当', order_date: '2025-02-09', order_quantity: 560 },
+  { product_code: '111111', name: 'サンプル弁当', order_date: '2025-02-08', order_quantity: 1440 },
+  { product_code: '111111', name: 'サンプル弁当', order_date: '2025-02-07', order_quantity: 1020 },
+  { product_code: '111111', name: 'サンプル弁当', order_date: '2025-02-06', order_quantity: 1680 }
+]
+
+items_data.each do |item_data|
+  Item.create!(item_data)
+end
